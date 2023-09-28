5013d7f5683e9b7acc8e7bae2382269a5671ddb2
Pertsaa <65128229+Pertsaa@users.noreply.github.com>
2023-09-28 12:44:48 +0300

    fix: state array rerender

--- src/routes/state-local/+page.svelte
+++ src/routes/state-local/+page.svelte
@@ -1,5 +1,6 @@
-<script>
+<script lang="ts">
   let count = 0;
+  let array: number[] = [];
 </script>
 
 <section class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -10,6 +11,18 @@
       on:click={() => (count += 1)}
       class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
       >Increment
+    </button>
+  </div>
+  <div class="bg-green-200 p-4">
+    <h1 class="text-lg">[{array}]</h1>
+    <button
+      type="button"
+      on:click={() => {
+        array.push(Math.floor(Math.random() * 10));
+        array = array;
+      }}
+      class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+      >Append
     </button>
   </div>
 </section>
